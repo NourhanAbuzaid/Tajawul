b066243bb88a8e537d9d7cfe7dee5c0398cbbea5
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-02-02 22:45:41 +0200

    Login and Signup

---
+++ app/login/page.js
@@ -0,0 +1,129 @@
+"use client";
+
+import styles from "../login.module.css";
+import GoogleIcon from "@mui/icons-material/Google";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import axios from "axios";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await axios.post(
+        "http://tajawul.runasp.net/api/Auth/signin",
+        {
+          email,
+          password,
+        }
+      );
+
+      console.log("Login successful:", response.data);
+    } catch (err) {
+      setError(err.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* Header: Logo */}
+      <div className={styles.left}>
+        <div className={styles.leftContent}>
+          <Image
+            src="/tajawul-logo-text-only.svg"
+            alt="Tajawul Logo"
+            width={120}
+            height={120}
+          />
+          {/* Form Container */}
+          <div className={styles.formContainer}>
+            <p className={styles.title}>Welcome Back</p>
+
+            {/* Google Authentication */}
+            <button className={styles.googleButton}>
+              <GoogleIcon /> Continue with Google
+            </button>
+
+            <div className={styles.divider}>
+              <span>Or Using Email</span>
+            </div>
+
+            {/* Start Form Items */}
+            <form onSubmit={handleSubmit} className={styles.formWidth}>
+              <label>
+                Email Address
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="Email Address"
+                />
+              </label>
+
+              <label className={styles.passwordLabel}>
+                Password
+                <div className={styles.passwordContainer}>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    placeholder="Password"
+                  />
+                  <button
+                    type="button"
+                    className={styles.togglePassword}
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </button>
+                </div>
+              </label>
+
+              {/* Error message display */}
+              {error && <p className={styles.errorMessage}>{error}</p>}
+
+              <Link href="../forgot-password" className={styles.Link}>
+                <p>Forgot Password?</p>
+              </Link>
+              <button type="submit" className={styles.submitButton}>
+                Log In
+              </button>
+            </form>
+
+            <div className={styles.divider}>
+              <span>Not a Member?</span>
+            </div>
+            <p className={styles.registerText}>
+              <Link href="../sign-up" className={styles.Link}>
+                Register
+              </Link>
+              &nbsp;to unlock the best of Tajawul
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.right}>
+        <Image
+          src="/GRAND_MOSQUE.jpg"
+          alt="The Grand Mosque in United Arab Emirates"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+    </div>
+  );
+}
---
+++ app/sign-up/page.js
@@ -0,0 +1,170 @@
+"use client";
+
+import styles from "../login.module.css";
+import GoogleIcon from "@mui/icons-material/Google";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import axios from "axios";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function SignUpPage() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "http://tajawul.runasp.net/api/Auth/signup",
+        {
+          firstName,
+          lastName,
+          email,
+          password,
+          confirmPassword,
+        }
+      );
+
+      console.log("Signup successful:", response.data);
+    } catch (err) {
+      setError(err.response?.data?.message || "Signup failed");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.right}>
+        <Image
+          src="/GRAND_MOSQUE.jpg"
+          alt="The Grand Mosque in United Arab Emirates"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+      {/* Header: Logo */}
+      <div className={styles.left}>
+        <div className={styles.leftContent}>
+          <Image
+            src="/tajawul-logo-text-only.svg"
+            alt="Tajawul Logo"
+            width={100}
+            height={80}
+          />
+          {/* Form Container */}
+          <div className={styles.formContainer}>
+            <div>
+              <p className={styles.title}>Create New Account</p>
+              <p className={styles.subtitle}>
+                Join us to unlock travel inspiration, personalized
+                Recommendations, and connections with fellow explorers!
+              </p>
+            </div>
+
+            {/* Start Form Items */}
+            <form onSubmit={handleSubmit} className={styles.formWidth}>
+              <div className={styles.nameContainer}>
+                <label>
+                  First Name
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                    placeholder="First Name"
+                  />
+                </label>
+                <label>
+                  Last Name
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                    placeholder="Last Name"
+                  />
+                </label>
+              </div>
+              <label>
+                Email Address
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="Email Address"
+                />
+              </label>
+
+              <label className={styles.passwordLabel}>
+                Password
+                <div className={styles.passwordContainer}>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    placeholder="Password"
+                  />
+                  <button
+                    type="button"
+                    className={styles.togglePassword}
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </button>
+                </div>
+              </label>
+
+              {/* Confirm Password */}
+              <label className={styles.passwordLabel}>
+                Confirm Password
+                <div className={styles.passwordContainer}>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                    placeholder="Confirm Password"
+                  />
+                </div>
+              </label>
+
+              {/* Error message display */}
+              {error && <p className={styles.errorMessage}>{error}</p>}
+
+              <button type="submit" className={styles.submitButton}>
+                Register Now
+              </button>
+            </form>
+
+            <div className={styles.divider}>
+              <span>Already a member?</span>
+            </div>
+            <p className={styles.registerText}>
+              <Link href="../login" className={styles.Link}>
+                Log In
+              </Link>
+              &nbsp;using your Tajawul account
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
