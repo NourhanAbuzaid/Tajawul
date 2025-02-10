a3480b734385ee28a4f0dfa687a20c4c4d1df7b3
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-02-10 21:12:24 +0200

    Fix z-index Issue

--- app/page.js
+++ app/page.js
@@ -6,24 +6,6 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Button type="primary" size="px14">
-        Primary 14px
-      </Button>
-      <Button type="secondary" size="px14">
-        Primary 14px
-      </Button>
-      <Button type="gradient" size="px14">
-        Primary 14px
-      </Button>
-      <Button type="primary" size="px16">
-        Primary 16px
-      </Button>
-      <Button type="secondary" size="px16">
-        Primary 16px
-      </Button>
-      <Button type="gradient" size="px16">
-        Primary 16px
-      </Button>
     </div>
   );
 }
