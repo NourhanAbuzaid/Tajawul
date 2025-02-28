109d376e4b3951695a5ec75a077723a723fcf4dc
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-02-28 02:56:27 +0200

    First attempt in Onboarding

--- app/page.js
+++ app/page.js
@@ -1,15 +1,29 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./page.module.css";
 import NavBar from "./components/ui/NavBar";
-import Button from "./components/ui/Button";
+import styles from "@/home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <NavBar />
-      <h1>Home</h1>
+      <div className={styles.container}>
+        <main>
+          <div className={styles.coverWrapper}>
+            <div className={styles.mainCover}>
+              <Image
+                src="/Egypt.jpg"
+                alt="Luxor, Egypt"
+                fill={true}
+                style={{ objectFit: "cover" }}
+                priority
+              />
+            </div>
+          </div>
+          <div className={styles.sectionBelowImage}> </div>
+        </main>
+      </div>
     </div>
   );
 }
