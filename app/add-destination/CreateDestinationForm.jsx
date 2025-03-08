fca178ad0642404571a5dc3650601521ca07fa18
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-03-08 23:12:15 +0200

    Test

--- app/add-destination/CreateDestinationForm.jsx
+++ app/add-destination/CreateDestinationForm.jsx
@@ -344,7 +344,7 @@ export default function CreateDestinationForm() {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `${accessToken}`,
+            Authorization: `Bearer ${accessToken}`,
           },
         }
       );
