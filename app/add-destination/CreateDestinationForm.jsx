ee933ecc0203c7c81ac3f62079dccd7f7437d3a9
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-03-08 23:01:25 +0200

    Remove Bearer

--- app/add-destination/CreateDestinationForm.jsx
+++ app/add-destination/CreateDestinationForm.jsx
@@ -344,7 +344,7 @@ export default function CreateDestinationForm() {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
+            Authorization: `${accessToken}`,
           },
         }
       );
