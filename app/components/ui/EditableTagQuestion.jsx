263c45949f18327557a0cbbc50a4abd63c23d86f
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-04-12 14:29:58 +0200

    Updating/Deleting Tags Logic (Done)

--- app/components/ui/EditableTagQuestion.jsx
+++ app/components/ui/EditableTagQuestion.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import styles from "./TagQuestion.module.css";
 import CloseIcon from "@mui/icons-material/Close";
+import Divider from "@mui/material/Divider";
 
 export default function EditableTagQuestion({
   question,
@@ -45,32 +46,15 @@ export default function EditableTagQuestion({
     onDelete({ target: { name: question, value: newDeletedTags } });
   };
 
-  const organizedOptions = () => {
-    // Create a Set of all unique option values from props
-    const allOptionValues = new Set(options.map((opt) => opt.value));
+  // Separate selected and unselected options
+  const selectedOptions = selectedTags.map((tag) => {
+    const optionFromProps = options.find((opt) => opt.value === tag);
+    return optionFromProps || { value: tag, label: tag };
+  });
 
-    // First: Show currently selected options (maintaining order)
-    const selectedOptions = selectedTags.map((tag) => {
-      // Find in options first
-      const optionFromProps = options.find((opt) => opt.value === tag);
-      // If not found in props, create a basic option
-      return optionFromProps || { value: tag, label: tag };
-    });
-
-    // Second: Show available options that aren't selected
-    const unselectedOptions = options
-      .filter((option) => !selectedTags.includes(option.value))
-      // Include originally selected but deselected options
-      .concat(
-        selectedValues
-          .filter(
-            (tag) => !selectedTags.includes(tag) && !allOptionValues.has(tag)
-          )
-          .map((tag) => ({ value: tag, label: tag }))
-      );
-
-    return [...selectedOptions, ...unselectedOptions];
-  };
+  const unselectedOptions = options.filter(
+    (option) => !selectedTags.includes(option.value)
+  );
 
   return (
     <div className={`${styles.questionContainer} ${styles[size]}`}>
@@ -78,34 +62,65 @@ export default function EditableTagQuestion({
         {question} {required && <span className={styles.requiredMark}>*</span>}
       </label>
 
-      <div className={styles.optionsContainer}>
-        {organizedOptions().map((option) => {
-          const isSelected = selectedTags.includes(option.value);
-          const wasOriginallySelected = selectedValues.includes(option.value);
-          const isDeleted = wasOriginallySelected && !isSelected;
+      {/* Selected options section */}
+      {selectedOptions.length > 0 && (
+        <div className={styles.selectedSection}>
+          <div className={styles.optionsContainer}>
+            {selectedOptions.map((option) => {
+              const wasOriginallySelected = selectedValues.includes(
+                option.value
+              );
+              const isDeleted =
+                wasOriginallySelected && !selectedTags.includes(option.value);
 
-          return (
-            <button
-              key={option.value}
-              className={`${styles.optionButton} ${
-                isSelected ? styles.selected : ""
-              } ${isDeleted ? styles.deleted : ""}`}
-              onClick={() => !disabled && handleTagClick(option.value)}
-              disabled={disabled}
-            >
-              <span className={styles.iconContainer}>
-                {option.icon && option.icon}
-                {option.label}
-                {isSelected && (
-                  <span className={styles.closeIcon}>
-                    <CloseIcon fontSize="small" />
+              return (
+                <button
+                  key={option.value}
+                  className={`${styles.optionButton} ${styles.selected} ${
+                    isDeleted ? styles.deleted : ""
+                  }`}
+                  onClick={() => !disabled && handleTagClick(option.value)}
+                  disabled={disabled}
+                >
+                  <span className={styles.iconContainer}>
+                    {option.icon && option.icon}
+                    {option.label}
+                    <span className={styles.closeIcon}>
+                      <CloseIcon fontSize="small" />
+                    </span>
                   </span>
-                )}
-              </span>
-            </button>
-          );
-        })}
-      </div>
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      )}
+
+      {/* Divider between sections */}
+      {selectedOptions.length > 0 && unselectedOptions.length > 0 && (
+        <Divider sx={{ width: "100%", my: 1 }} />
+      )}
+
+      {/* Unselected options section */}
+      {unselectedOptions.length > 0 && (
+        <div className={styles.unselectedSection}>
+          <div className={styles.optionsContainer}>
+            {unselectedOptions.map((option) => (
+              <button
+                key={option.value}
+                className={styles.optionButton}
+                onClick={() => !disabled && handleTagClick(option.value)}
+                disabled={disabled}
+              >
+                <span className={styles.iconContainer}>
+                  {option.icon && option.icon}
+                  {option.label}
+                </span>
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
