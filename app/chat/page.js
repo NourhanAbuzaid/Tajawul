fc8a9b8d9065791de6e84c84cba664329c257149
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-06-09 18:19:15 +0300

    Initial Chatbot Page

--- app/chat/page.js
+++ app/chat/page.js
@@ -2,14 +2,26 @@
 
 import NavBar from "@/components/ui/NavBar";
 import withAuth from "@/utils/withAuth";
+import AddIcon from "@mui/icons-material/Add";
+import styles from "@/styles/Chatbot.module.css";
 
 function Chat() {
   return (
     <div>
       <NavBar />
-      <h1>Chat Page</h1>
-      <p>This is the chat page where you can interact with the chatbot.</p>
-      {/* Additional chat components and functionality will go here */}
+      <div className={styles.chatPageContainer}>
+        <div className={styles.chatHistoryColumn}>
+          <button className={styles.newChatButton}>
+            <AddIcon /> New Chat
+          </button>
+          {/* Chat history */}
+        </div>
+        <div className={styles.chatInputColumn}>
+          <h2>Chat Input</h2>
+          <p>Here you can type your messages to the chatbot.</p>
+          {/* Chat input field and send button will go here */}
+        </div>
+      </div>
     </div>
   );
 }
