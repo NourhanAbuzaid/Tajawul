403fd9b22eb23696ee162bb7b63fdc87409f32f8
NourhanAbuzaid <62885596+NourhanAbuzaid@users.noreply.github.com>
2025-06-09 18:43:59 +0300

    Add Chatbot FAB for every suitable page

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import { AnimatePresence } from "framer-motion";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { dmSans, merriweather } from "./fonts";
-import FloatingChatButton from "@/components/ui/FloatingChatButton";
 
 export const metadata = {
   title: "Tajawul",
@@ -15,7 +14,6 @@ export default function RootLayout({ children }) {
       <body>
         <AnimatePresence mode="wait">{children}</AnimatePresence>
         <SpeedInsights />
-        <FloatingChatButton />
       </body>
     </html>
   );
--- app/translate/layout.js
+++ app/translate/layout.js
@@ -1,4 +1,5 @@
 import NavBar from "@/components/ui/NavBar";
+import FloatingChatButton from "@/components/ui/FloatingChatButton"; // Import the floating chat button component
 
 export const metadata = {
   title: "Tajawul Translate",
@@ -8,6 +9,7 @@ export default function Layout({ children }) {
   return (
     <div>
       <NavBar /> {children}
+      <FloatingChatButton />
     </div>
   );
 }
